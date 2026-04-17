078cbaee762c19c55d76931d0513c0815c785e9a
shahzor khan <lamborghinia693@gmail.com>
2026-04-17 18:39:21 +0500

    add .env

--- lib/subabase.ts
+++ lib/subabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://jrhjfqlbpyogvmbdtcrz.supabase.co",
-  "sb_publishable_77DcY1bcdflhLttodoBn_A_FNFrrOsy"
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
